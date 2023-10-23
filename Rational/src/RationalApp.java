96319ed883f8e5de91f36e341507e112d0e67a5d
6410685157 <6410685157@student.tu.ac.th>
2023-10-23 14:48:58 +0700

    to bigint

--- Rational/src/RationalApp.java
+++ Rational/src/RationalApp.java
@@ -1,17 +1,17 @@
 import java.math.BigInteger;
 public class RationalApp{
-  public static void main(String[] args, BigInteger A, BigInteger B, BigInteger x0, BigInteger x1) {
-    A  = BigInteger.valueOf(0);
-    B  = BigInteger.valueOf(1); 
+  public static void main(String[] args) {
+    BigInteger A  = BigInteger.valueOf(0);
+    BigInteger B  = BigInteger.valueOf(1); 
     Rational sum = new Rational(A, B);
-        for (int i = 1; i <= 99; i++) {
-            x0 = BigInteger.valueOf(i);
-            x1 = BigInteger.valueOf(i + 1);
+        for (int i = 99; i >= 1; i--) {
+            BigInteger x0 = BigInteger.valueOf(i);
+            BigInteger x1 = BigInteger.valueOf(i + 1);
             Rational x = new Rational(x0, x1);
             sum = sum.add(x);
-            sum.reduce();
-            String result = sum.toString();
-            System.out.println(i + ": " + result);
         }
+    sum.reduce();
+    String result = sum.toString();
+    System.out.println("result: " + result);
     }
 }
