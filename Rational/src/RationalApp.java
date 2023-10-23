eb1952ee7a24d82925f8c933ece446df59bb2eb8
6410685157 <6410685157@student.tu.ac.th>
2023-10-23 14:52:17 +0700

    final

--- Rational/src/RationalApp.java
+++ Rational/src/RationalApp.java
@@ -4,14 +4,14 @@ public class RationalApp{
     BigInteger A  = BigInteger.valueOf(0);
     BigInteger B  = BigInteger.valueOf(1); 
     Rational sum = new Rational(A, B);
-        for (int i = 99; i >= 1; i--) {
+        for (int i = 1; i <= 99; i++) {
             BigInteger x0 = BigInteger.valueOf(i);
             BigInteger x1 = BigInteger.valueOf(i + 1);
             Rational x = new Rational(x0, x1);
             sum = sum.add(x);
+            sum.reduce();
+            String result = sum.toString();
+            System.out.println(i + ": " + result);
         }
-    sum.reduce();
-    String result = sum.toString();
-    System.out.println("result: " + result);
     }
 }
